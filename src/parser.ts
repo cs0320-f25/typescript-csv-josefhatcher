c928bdb1e901d324f43cf506758ba1d307f801d4
josef hatcher <josef_hatcher@brown.edu>
2025-09-14 11:50:26 -0400

    Create parser.ts
    new parser that works

---
+++ src/parser.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+import * as readline from "readline";
+import { ZodType } from "zod";
+
+/**
+ * This is a JSDoc comment. Similar to JavaDoc, it documents a public-facing
+ * function for others to use. Most modern editors will show the comment when 
+ * mousing over this function name. Try it in run-parser.ts!
+ * 
+ * File I/O in TypeScript is "asynchronous", meaning that we can't just
+ * read the file and return its contents. You'll learn more about this 
+ * in class. For now, just leave the "async" and "await" where they are. 
+ * You shouldn't need to alter them.
+ * 
+ * @param path The path to the file being loaded.
+ * @returns a "promise" to produce a 2-d array of cell values
+ */
+export async function parseCSV<T>(path: string, schema: ZodType<T>): Promise<string[][] | T[]> {
+
+  const fileStream = fs.createReadStream(path);
+  const rl = readline.createInterface({
+
+
+
+    input: fileStream,
+    crlfDelay: Infinity, // handle different line endings
+  });
+  
+  // Create an empty array to hold the results
+  const result: (string[] | T)[] = [];
+  
+  // We add the "await" here because file I/O is asynchronous. 
+  // We need to force TypeScript to _wait_ for a row before moving on. 
+  // More on this in class soon!
+  for await (const line of rl) {
+    const values = line.split(",").map((v) => v.trim());
+
+    if (!schema){
+      result.push(values);
+    } else {
+      const parsed = schema.safeParse(values);
+      if (!parsed.success){
+        throw new Error(`CSV row failed validation: ${parsed.error.message}`);
+      }
+      result.push(parsed.data);
+    }
+  }
+  return result as string[][] | T[];
+}
+
